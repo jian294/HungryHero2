acdde98972ea05b9711b7ddb338b0dae9dedc36f
jian294 <jian294@163.com>
2014-06-27 18:56:19 +0800

    proj files
    proj files

---
+++ proj.android/gen/R.java.d
@@ -0,0 +1,6 @@
+E:\cocos2dxProj\HungryHero\proj.android\gen\com\lightspeed\HungryHero2\R.java \
+ : E:\cocos2dxProj\HungryHero\proj.android\res\drawable-hdpi\icon.png \
+E:\cocos2dxProj\HungryHero\proj.android\res\drawable-ldpi\icon.png \
+E:\cocos2dxProj\HungryHero\proj.android\res\drawable-mdpi\icon.png \
+E:\cocos2dxProj\HungryHero\proj.android\res\values\strings.xml \
+E:\cocos2dxProj\HungryHero\proj.android\bin\AndroidManifest.xml \
